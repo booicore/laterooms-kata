0000b668848a9437210e8888ec334c372b812aed
Julian Rosebury <julian.rosebury@kalibrate.com>
2017-06-16 10:49:25 +0100

    Should now compile and the test 'Get_should_return_a_valid_total' should fail

---
+++ CheckoutKata/CheckoutKata/Helpers/GrandTotal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using CheckoutKata.Interfaces.Helpers;
+
+namespace CheckoutKata.Helpers
+{
+    public class GrandTotal : IGrandTotal
+    {
+        public decimal GetGrandTotal(Dictionary<string, int> scanned)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- CheckoutKata/CheckoutKataTests/Helpers/GrandTotalTests.cs
+++ CheckoutKata/CheckoutKataTests/Helpers/GrandTotalTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using CheckoutKata.Helpers;
 using NUnit.Framework;
 
 namespace CheckoutKataTests.Helpers
@@ -24,9 +25,11 @@ namespace CheckoutKataTests.Helpers
         [Test]
         public void Get_should_return_a_valid_total()
         {
+            var scanned = new Dictionary<string, int>();
+
             var sut = CreateSUT();
 
-            var response = sut.GetGrandTotal();
+            var response = sut.GetGrandTotal(scanned);
 
             Assert.IsTrue(response >= 0.0m);
 
