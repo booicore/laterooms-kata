3d6984e01b7b639594d9feb10e7a6f23d66dac56
Julian Rosebury <julian.rosebury@kalibrate.com>
2017-06-16 14:59:35 +0100

    GetTotalPricePerSku test goes green and some extra test coverage added alongside

--- CheckoutKata/CheckoutKata/Helpers/GetTotalPricePerSku.cs
+++ CheckoutKata/CheckoutKata/Helpers/GetTotalPricePerSku.cs
@@ -4,16 +4,22 @@ namespace CheckoutKata.Helpers
 {
     public class GetTotalPricePerSku : IGetTotalPricePerSku
     {
+        private IGetNonPromotionalPrice _getNonPromotionalPrice;
+        private IGetPromotionalPrice _getPromotionalPrice;
+
+        public GetTotalPricePerSku(IGetNonPromotionalPrice getNonPromotionalPrice, IGetPromotionalPrice getPromotionalPrice)
+        {
+            _getNonPromotionalPrice = getNonPromotionalPrice;
+            _getPromotionalPrice = getPromotionalPrice;
+        }
+
         public decimal GetPrice(string sku, int quantity)
         {
-            //TODO: Get the promotional cost 
-
-            //TODO:  get the non-promotional cost
-
-
-            //TODO:  add the two totals together
-
-            throw new System.NotImplementedException();
+            var promoprice = _getPromotionalPrice.Get(sku, quantity);
+            
+            var nonpromoprice = _getNonPromotionalPrice.Get(sku, quantity);
+            
+            return promoprice + nonpromoprice; 
         }
     }
 }
--- CheckoutKata/CheckoutKataTests/Helpers/GetTotalPricePerSkuTests.cs
+++ CheckoutKata/CheckoutKataTests/Helpers/GetTotalPricePerSkuTests.cs
@@ -2,6 +2,8 @@
 using System.Text;
 using System.Collections.Generic;
 using CheckoutKata.Helpers;
+using CheckoutKata.Interfaces.Helpers;
+using Moq;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
@@ -11,15 +13,19 @@ namespace CheckoutKataTests.Helpers
     [TestFixture()]
     public class GetTotalPricePerSkuTests
     {
+        private Mock<IGetPromotionalPrice> _mockGetPromotionalPrice;
+        private Mock<IGetNonPromotionalPrice> _mockGetNonPromotionalPrice;
+
         [SetUp]
         public void SetUp()
         {
-            
+            _mockGetPromotionalPrice = new Mock<IGetPromotionalPrice>();
+            _mockGetNonPromotionalPrice = new Mock<IGetNonPromotionalPrice>();
         }
 
         private GetTotalPricePerSku CreateSUT()
         {
-            return new GetTotalPricePerSku();
+            return new GetTotalPricePerSku(_mockGetNonPromotionalPrice.Object, _mockGetPromotionalPrice.Object);
         }
 
         [Test]
@@ -27,11 +33,54 @@ namespace CheckoutKataTests.Helpers
         {
             var expected = 230m;
 
+            var quantity = 5;
+
             var sku = "A";
+
+            _mockGetNonPromotionalPrice.Setup(x => x.Get(sku, quantity)).Returns(100m);
+            _mockGetPromotionalPrice.Setup(x => x.Get(sku, quantity)).Returns(130m);
 
             var sut = CreateSUT();
 
-            var response = sut.GetPrice(sku, 5);
+            var response = sut.GetPrice(sku, quantity);
+
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test]
+        public void GetPrice_should_return_a_total_price_per_product_type_when_nonpromo_is_zero()
+        {
+            var expected = 130m;
+
+            var quantity = 3;
+
+            var sku = "A";
+
+            _mockGetNonPromotionalPrice.Setup(x => x.Get(sku, quantity)).Returns(0m);
+            _mockGetPromotionalPrice.Setup(x => x.Get(sku, quantity)).Returns(130m);
+
+            var sut = CreateSUT();
+
+            var response = sut.GetPrice(sku, quantity);
+
+            Assert.AreEqual(expected, response);
+        }
+
+        [Test]
+        public void GetPrice_should_return_a_total_price_per_product_type_only_nonpromo()
+        {
+            var expected = 50m;
+
+            var quantity = 1;
+
+            var sku = "A";
+
+            _mockGetNonPromotionalPrice.Setup(x => x.Get(sku, quantity)).Returns(50m);
+            _mockGetPromotionalPrice.Setup(x => x.Get(sku, quantity)).Returns(0m);
+
+            var sut = CreateSUT();
+
+            var response = sut.GetPrice(sku, quantity);
 
             Assert.AreEqual(expected, response);
         }
