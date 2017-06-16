6f553a36690161909839825996b7dcd58b0f6459
Julian Rosebury <julian.rosebury@kalibrate.com>
2017-06-16 11:42:31 +0100

    Set up GetGrandTotal test to expect a correct total, psuedo-code the steps in the GrandTotal Class to give a rough idea of how we might solve this

--- CheckoutKata/CheckoutKata/Helpers/GrandTotal.cs
+++ CheckoutKata/CheckoutKata/Helpers/GrandTotal.cs
@@ -7,7 +7,17 @@ namespace CheckoutKata.Helpers
     {
         public decimal GetGrandTotal(Dictionary<string, int> scanned)
         {
-            throw new System.NotImplementedException();
+            var grandTotal = 0.0m;
+
+            // loop through scanned 
+
+            // get a product price per product and calculate the promotion price total and any single price totals
+                // add them together: promotionTotal + singlesTotal
+                // predicting using Modulus here
+
+            // get totals for each product type and add them to the grandTotal
+
+            return grandTotal;
         }
     }
 }
--- CheckoutKata/CheckoutKataTests/Helpers/GrandTotalTests.cs
+++ CheckoutKata/CheckoutKataTests/Helpers/GrandTotalTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using CheckoutKata.DAL;
 using CheckoutKata.Helpers;
 using NUnit.Framework;
 
@@ -33,6 +34,28 @@ namespace CheckoutKataTests.Helpers
 
             Assert.IsTrue(response >= 0.0m);
 
+        }
+
+        [Test]
+        public void Get_should_add_up_the_products_cost_using_the_prices_set_in_Products_stub_data()
+        {
+            var getProductsStubData = new GetProductsStubData();
+
+            var stubData = getProductsStubData.GetProductsData();
+
+            var scanned = new Dictionary<string, int>();
+
+            scanned.Add("A", 4); // 180m
+            scanned.Add("B", 2); // 45m
+            scanned.Add("D", 4); // 60m
+
+            var expected = 285.0m;
+
+            var sut = CreateSUT();
+
+            var response = sut.GetGrandTotal(scanned);
+
+            Assert.AreEqual(expected, response);
         }
     }
 }
