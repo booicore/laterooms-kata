4b1d32ce4d73e713d1d75bf02b61f16592f09e22
Julian Rosebury <julian.rosebury@kalibrate.com>
2017-06-16 09:29:41 +0100

    Test for a forthcoming AddToScanned class

---
+++ CheckoutKata/CheckoutKata/Interfaces/Helpers/IAddToScannedItems.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CheckoutKata.Interfaces.Helpers
+{
+    public interface IAddToScannedItems
+    {
+        Dictionary<string, int> AddToScanned(Dictionary<string, int> scanned, string sku);
+    }
+}
---
+++ CheckoutKata/CheckoutKataTests/Helpers/AddToScannedItemsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CheckoutKataTests.Helpers
+{
+
+    [TestFixture]
+    public class AddToScannedItemsTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        private AddToScannedItems CreateSUT()
+        {
+            return new AddToScannedItems();
+        }
+
+        [Test]
+        public void SortScannedItems_should_return_item_totals_per_product()
+        {
+            var skuA = "A";
+            var skuB = "B";
+
+            var scanned = new Dictionary<string, int>();
+
+            scanned.Add(skuA, 4);
+            scanned.Add(skuB, 2);
+
+            var sut = CreateSUT();
+
+            var response = sut.AddToScanned(scanned, skuB);
+
+            Assert.AreEqual(99, response[1].Value);
+        }
+    }
+}
