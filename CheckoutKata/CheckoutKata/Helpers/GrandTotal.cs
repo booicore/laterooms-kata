c51e8cc0091befa67936ee1e4c469e1aae642ff6
Julian Rosebury <julian.rosebury@kalibrate.com>
2017-06-16 12:09:43 +0100

    A little bit more thinking-out-loud in GetGrandTotal method before the next test/step

--- CheckoutKata/CheckoutKata/Helpers/GrandTotal.cs
+++ CheckoutKata/CheckoutKata/Helpers/GrandTotal.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections.Generic;
+using CheckoutKata.Interfaces.DAL;
 using CheckoutKata.Interfaces.Helpers;
 
 namespace CheckoutKata.Helpers
 {
     public class GrandTotal : IGrandTotal
     {
+        
         public decimal GetGrandTotal(Dictionary<string, int> scanned)
         {
             var grandTotal = 0.0m;
 
             // loop through scanned 
+            foreach (var skuAndTotalPerSku in scanned)
+            {
+                // we need a GetTotalPricePerSku(skuAndTotalPerSku) class - 
 
-            // get a product price per product and calculate the promotion price total and any single price totals
-                // add them together: promotionTotal + singlesTotal
-                // predicting using Modulus here
+                    // get a product price per product, plus calculate the promotion price total and any single price totals
+                    // add them together: promotionTotal + singlesTotal
+                    // predicting using Modulus here
 
-            // get totals for each product type and add them to the grandTotal
-
+                // get totals for each product type and add them to the grandTotal
+            }
+            
             return grandTotal;
         }
     }
